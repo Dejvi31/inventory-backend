f70cc9eb94faeed7708ef12d70bef5999a9a96d5
Dejvi <dejviepadejvi@gmail.com>
2023-12-27 14:08:55 +0100

    krijuam protect middleware dhe user get data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,6 +114,7 @@ const registerUser = asyncHandler( async (req,res) => {
 
    })
 
+   //Logout
    const logout = asyncHandler( async (req,res) => {
       res.cookie("token", "", {
          path: "/",
@@ -126,4 +127,24 @@ const registerUser = asyncHandler( async (req,res) => {
    })
 
 
-module.exports = {registerUser,loginUser, logout}
+   //Get User Data
+   const getUser = asyncHandler( async (req,res) => {
+      const user = await User.findById(req.user._id)
+
+      if(user) {
+         const {_id, name, email, photo, phone, bio} = user
+         res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio
+         })
+      } else {
+         res.status(400)
+         throw new Error("User Not Found")
+      }
+   })
+
+module.exports = {registerUser,loginUser, logout, getUser}
